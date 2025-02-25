38021bae588f238141b8d1b4acd629406b323e87
theamarverma <amarverma.lnct@gmail.com>
2025-02-25 18:17:38 +0530

    add FindUs component to display partner icons with layout and styling

---
+++ src/components/Home/FindUs.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import React from "react";
+
+const FindUs = () => {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-10">
+      <h4 className="">Find us at</h4>
+      <div className="flex h-[154px] gap-6">
+        <Image
+          className="h-full"
+          src={"/images/homepage/icons/google.png"}
+          alt="google"
+          width={154}
+          height={154}
+        />
+        <Image
+          className="h-full"
+          src={"/images/homepage/icons/swiggy.png"}
+          alt="swiggy"
+          width={154}
+          height={600}
+        />
+        <Image
+          className="h-full"
+          src={"/images/homepage/icons/zomato.png"}
+          alt="zomato"
+          width={154}
+          height={600}
+        />
+        <Image
+          className="h-full"
+          src={"/images/homepage/icons/trip.png"}
+          alt="trip advisor"
+          width={154}
+          height={600}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FindUs;
