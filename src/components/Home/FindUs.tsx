3b58c196c2bb25d2c20cf645e86de058680118e2
theamarverma <amarverma.lnct@gmail.com>
2025-03-13 19:31:18 +0530

    feat: add external links to images in FindUs component

--- src/components/Home/FindUs.tsx
+++ src/components/Home/FindUs.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const FindUs = () => {
@@ -6,34 +7,61 @@ const FindUs = () => {
     <div className="flex flex-col items-center justify-center space-y-10">
       <h1 className="text-xl md:text-header">Find us at</h1>
       <div className="mx-4 flex gap-3 md:mx-0 md:h-[154px] md:gap-6">
-        <Image
-          className="h-full"
-          src={"/images/homepage/icons/google.png"}
-          alt="google"
-          width={154}
-          height={154}
-        />
-        <Image
-          className="h-full"
-          src={"/images/homepage/icons/swiggy.png"}
-          alt="swiggy"
-          width={154}
-          height={600}
-        />
-        <Image
-          className="h-full"
-          src={"/images/homepage/icons/zomato.png"}
-          alt="zomato"
-          width={154}
-          height={600}
-        />
-        <Image
-          className="h-full"
-          src={"/images/homepage/icons/trip.png"}
-          alt="trip advisor"
-          width={154}
-          height={600}
-        />
+        <Link
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="h-full"
+            src={"/images/homepage/icons/google.png"}
+            alt="google"
+            width={154}
+            height={154}
+          />
+        </Link>
+
+        <Link
+          href="https://www.swiggy.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="h-full"
+            src={"/images/homepage/icons/swiggy.png"}
+            alt="swiggy"
+            width={154}
+            height={600}
+          />
+        </Link>
+
+        <Link
+          href="https://www.zomato.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="h-full"
+            src={"/images/homepage/icons/zomato.png"}
+            alt="zomato"
+            width={154}
+            height={600}
+          />
+        </Link>
+
+        <Link
+          href="https://www.tripadvisor.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="h-full"
+            src={"/images/homepage/icons/trip.png"}
+            alt="trip advisor"
+            width={154}
+            height={600}
+          />
+        </Link>
       </div>
     </div>
   );
